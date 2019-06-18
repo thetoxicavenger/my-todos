0a11e6fa2416a37c375f9d4d765d7cb87ac3f81b
Mitch Cravens <mitch.cravens@galvanize.com>
2019-06-18 14:59:38 -0700

    yea

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require("express");
-const path = require("path");
-const app = express();
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 8000;
+
+const app = express();
+const port = parseInt(process.env.PORT, 10) || 8000
+
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.set('view engine', 'ejs');
 
 var routes_setter = require('./config/routes.js');
 routes_setter(app);
@@ -16,3 +13,41 @@ routes_setter(app);
 app.listen(port, function() {
   console.log('Listening on', port);
 });
+
+/* 
+
+const express = require('express')
+const next = require('next')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  const server = express()
+
+  server.get('/a', (req, res) => {
+    return app.render(req, res, '/a', req.query)
+  })
+
+  server.get('/b', (req, res) => {
+    return app.render(req, res, '/b', req.query)
+  })
+
+  server.get('/posts/:id', (req, res) => {
+    return app.render(req, res, '/posts', { id: req.params.id })
+  })
+
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(port, err => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
+
+
+*/
